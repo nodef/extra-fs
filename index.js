895840d0bbb69680925da039525b1b02e6c123d1
Subhajit Sahu <wolfram77@gmail.com>
2019-04-06 21:37:46 +0530

    fix: no such directory, needed to change the rename order

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ const path = require('path');
 
 function dehuskDirSync(dir, depth=-1) {
   for(var seed=dir; depth; depth--) {
-    var ents = fs.readdirSync(dir, {withFileTypes: true});
+    var ents = fs.readdirSync(seed, {withFileTypes: true});
     if(ents.length===0 || ents.length>1 || ents[0].isFile()) break;
     seed = path.join(seed, ents[0].name);
   }
   if(seed===dir) return seed;
   var temp = dir+Math.random();
-  fs.moveSync(dir, temp);
-  fs.moveSync(seed, dir);
-  fs.removeSync(temp);
+  fs.moveSync(seed, temp);
+  fs.removeSync(dir);
+  fs.moveSync(temp, dir);
   return seed;
 }
 
 async function dehuskDir(dir, depth=-1) {
   for(var seed=dir; depth; depth--) {
-    var ents = await fs.readdir(dir, {withFileTypes: true});
+    var ents = await fs.readdir(seed, {withFileTypes: true});
     if(ents.length===0 || ents.length>1 || ents[0].isFile()) break;
     seed = path.join(seed, ents[0].name);
   }
   if(seed===dir) return seed;
   var temp = dir+Math.random();
-  await fs.move(dir, temp);
-  await fs.move(seed, dir);
-  await fs.remove(temp);
+  await fs.move(seed, temp);
+  await fs.remove(dir);
+  await fs.move(temp, dir);
   return seed;
 }
 fs.dehuskDirSync = dehuskDirSync;
