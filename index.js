e42caac70e155b53afbce3c588acf17ab375f9ad
Subhajit Sahu <wolfram77@gmail.com>
2019-04-06 21:27:20 +0530

    add dehuskDirSync with depth option

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const path = require('path');
 
 
 
-async function dehuskDir(dir) {
-  var ents = await fs.readdir(dir, {withFileTypes: true});
-  if(ents.length===0 || ents.length>1 || ents[0].isFile()) return;
-  var temp = dir+'.temp', seed = path.join(temp, ents[0].name);
+function dehuskDirSync(dir, depth=-1) {
+  for(var seed=dir; depth; depth--) {
+    var ents = fs.readdirSync(dir, {withFileTypes: true});
+    if(ents.length===0 || ents.length>1 || ents[0].isFile()) break;
+    seed = path.join(seed, ents[0].name);
+  }
+  if(seed===dir) return seed;
+  var temp = dir+Math.random();
+  fs.moveSync(dir, temp);
+  fs.moveSync(seed, dir);
+  fs.removeSync(temp);
+  return seed;
+}
+
+async function dehuskDir(dir, depth=-1) {
+  for(var seed=dir; depth; depth--) {
+    var ents = await fs.readdir(dir, {withFileTypes: true});
+    if(ents.length===0 || ents.length>1 || ents[0].isFile()) break;
+    seed = path.join(seed, ents[0].name);
+  }
+  if(seed===dir) return seed;
+  var temp = dir+Math.random();
   await fs.move(dir, temp);
   await fs.move(seed, dir);
   await fs.remove(temp);
+  return seed;
 }
+fs.dehuskDirSync = dehuskDirSync;
 fs.dehuskDir = dehuskDir;
 module.exports = fs;
