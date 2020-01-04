057f5108c7a37f0cc2f002a274e24609af65dd71
Subhajit Sahu <wolfram77@gmail.com>
2020-01-05 01:35:50 +0530

    :mushroom: preparing to add which

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const fs = require('fs-extra');
 const path = require('path');
+const os = require('os');
 
+const PATH = E['PATH']||'';
+const PATHEXT = (E['PATHEXT']||'').toLowerCase();
+const WIN32 = os.platform()==='win32';
+const PATHSEP = WIN32? ';':':';
+
+
+function which(prog) {
+  return new Promise((fres, frej) => _which(prog, fres));
+}
+function _which(prog, fn) {
+  var map = new Map();
+  var ps = PATH.split(PATHSEP);
+  for(var i=0, j=0, I=ps.length; i<I; i++) {
+    whichDir(prog, ps[i], map, err => {
+      if(++j>=I) fn(Array.from(map.values()));
+    });
+  }
+}
+function whichDir(prog, dir, map, fn) {
+  var isExe = WIN32? isExeWin32:isExeNix;
+  fs.readdir(dir, {withFileTypes: true}, (err, files) => {
+    if(err) return fn(err);
+    for(var f of files) {
+      if(!f.isFile()) continue;
+      var base = path.basename(f.name);
+      var p = path.join(dir, f.name);
+      var p0 = map.get(base)||null;
+      if(!prog.test(base)) continue;
+      if(isExe(p, p0)) map.set(base, p);
+    }
+    fn(null);
+  });
+}
+function isExeWin32(p1, p0) {
+  var i0 = PATHEXT.indexOf(path.extname(p0||'f.zzz').toLowerCase());
+  var i1 = PATHEXT.indexOf(path.extname(p1).toLowerCase());
+  return i1>=0 && (i0<0 || i1<i0);
+}
+function isExeNix(p1) {
+  return path.extname(p1)==='';
+}
 
 
 function dehuskDirSync(dir, depth=-1) {
